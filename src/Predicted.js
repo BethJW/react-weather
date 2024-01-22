c19fc11780c7ebf429af6dc97a31817ea638d1a0
BethJW <bethanyjwilliamson16@gmail.com>
2024-01-22 16:14:23 +0000

    added link to git repository

--- src/Predicted.js
+++ src/Predicted.js
@@ -1,10 +1,16 @@
 import React from "react";
-
+import "./Predicted.css";
 export default function Predicted() {
   return (
-    <div className="card-forecast">
-      <h2>Predicted Forecast</h2>
-      <div className="row" id="row"></div>
+    <div>
+      <div className="card-forecast">
+        <h2>Predicted Forecast</h2>
+        <div className="row" id="row"></div>
+      </div>
+      <div className="sign">
+        Coded by{" "}
+        <a href="https://github.com/BethJW/react-weather">Beth Williamson</a>
+      </div>
     </div>
   );
 }
